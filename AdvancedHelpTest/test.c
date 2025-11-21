ce8fff361a7d291a529a8d0b98114a769bcddb24
Sergio Molina Cardín <sergio.molina_cardin.ext@nokia.com>
2025-11-21 13:58:55 +0100

    Added a test for wchar_t version of the functions. Other minor improvvements.

--- AdvancedHelpTest/test.c
+++ AdvancedHelpTest/test.c
@@ -5,6 +5,15 @@
 #include <Windows.h>
 
 
+
+
+/////   DEFINES   /////
+
+#define MAX_KEYWORD_LEN 64
+
+
+
+
 /////   FUNCTION DEFINITIONS   /////
 
 int main(char* args);
@@ -16,10 +25,16 @@ void printOK();
 /////   FUNCTION IMPLEMENTATIONS   /////
 
 void printOK() {
-	printf(" __________________\n");
-	printf("|                  |\n");
-	printf("|    RESULT  OK    |\n");
-	printf("|__________________|\n");
+	printf(" ________________\n");
+	printf("|                |\n");
+	printf("|    TEST  OK    |\n");
+	printf("|________________|\n");
+}
+void printNOK() {
+	printf(" ________________\n");
+	printf("|                |\n");
+	printf("|   TEST  FAIL   |\n");
+	printf("|________________|\n");
 }
 
 
@@ -30,7 +45,7 @@ void printOK() {
 int main(char* args){
 	printf("TESTING AdvancedHelp module...\n");
 
-	char* keyword = "algo";
+	char keyword[MAX_KEYWORD_LEN] = "algo";
 	char* result = NULL;
 	int error = 0;
 
@@ -58,6 +73,16 @@ int main(char* args){
 	printf("\n");
 
 
+
+	///  TEST char FUNCTIONS  ///
+		////////////////////////////////////////////////////////////////////////////////////////////////////
+	printf("\n");
+	printf("=============================\n");
+	printf("||     TEST  1  STARTS     ||\n");
+	printf("=============================\n");
+	printf("\n");
+
+
 	// Init
 	printf("Before init:\n");
 	printf("help_ptr = %p\n", help_ptr);
@@ -70,7 +95,7 @@ int main(char* args){
 
 	// Use function getAdvancedHelpForKeyword()
 	result = getAdvancedHelpForKeyword(keyword, help_ptr);
-	printf("Obtained result:\n");
+	printf("Obtained result (len=%d):\n", (NULL == result) ? -1 : (int)strlen(result));
 	printf("--------------------------------------------------\n");
 	printf("%s", (NULL == result) ? "NULL" : result);
 	printf("--------------------------------------------------\n");
@@ -78,7 +103,7 @@ int main(char* args){
 	printf("After getTextFromFile:\n");
 	printf("error = %i\n", error);
 	printf("text_result_ptr = %p\n", help_ptr);
-	printf("Expected result:\n");
+	printf("Expected result (len=%d):\n", (NULL == text_result_ptr) ? -1 : (int)strlen(text_result_ptr));
 	printf("--------------------------------------------------\n");
 	printf("%s", (NULL == text_result_ptr) ? "NULL" : text_result_ptr);
 	printf("--------------------------------------------------\n");
@@ -91,6 +116,8 @@ int main(char* args){
 	//printf("0 == strcmp(result, text_result_ptr) --> %s\n", (0 == strcmp(result, text_result_ptr)) ? "true" : "false");
 	if (NULL != result && NULL != text_result_ptr && strlen(result) == strlen(text_result_ptr) && 0 == strcmp(result, text_result_ptr)) {
 		printOK();
+	} else {
+		printNOK();
 	}
 
 
@@ -114,5 +141,104 @@ int main(char* args){
 	printf("help_ptr = %p\n", help_ptr);
 	printf("\n");
 
-	//printf("help_ptr = %s\n", (NULL == help_ptr) ? "NULL" : help_ptr);
+
+
+	////////////////////////////////////////////////////////////////////////////////////////////////////
+	printf("\n\n\n");
+
+
+	///  TEST wchar_t FUNCTIONS  ///
+
+	printf("\n");
+	printf("=============================\n");
+	printf("||     TEST  2  STARTS     ||\n");
+	printf("=============================\n");
+	printf("\n");
+
+	//--------------------------------------------------------------------------------
+	// Example of how to transform char* into WCHAR*:
+	//--------------------------------------------------------------------------------
+	//WCHAR* help_filename_w = NULL;
+	//size_t help_filename_len = strlen(help_filename);
+	//help_filename_w = malloc(sizeof(WCHAR) * (help_filename_len + 1));
+	//if (NULL == help_filename_w) {
+	//	// ERROR
+	// }
+	//size_t res = mbstowcs(help_filename_w, help_filename, help_filename_len);
+	// // Check res
+	//help_filename_w[help_filename_len] = L'\0';
+	//--------------------------------------------------------------------------------
+
+	// Preparation of W variables
+	WCHAR* help_filename_w[MAX_PATH] = { 0 };
+	size_t conv_chars = 0;
+	error = mbstowcs_s(&conv_chars, help_filename_w, MAX_PATH, help_filename, MAX_PATH);
+
+	WCHAR* text_result_filename_w[MAX_PATH] = { 0 };
+	error = mbstowcs_s(&conv_chars, text_result_filename_w, MAX_PATH, text_result_filename, MAX_PATH);
+
+	WCHAR* keyword_w[MAX_KEYWORD_LEN] = { 0 };
+	error = mbstowcs_s(&conv_chars, keyword_w, MAX_KEYWORD_LEN, keyword, MAX_KEYWORD_LEN);
+
+	WCHAR* result_w = NULL;
+	WCHAR* text_result_ptr_w = NULL;
+
+	// Init
+	printf("Before init:\n");
+	printf("help_ptr = %p\n", help_ptr);
+	error = initAdvancedHelpW(help_filename_w, &help_ptr);
+	printf("After init:\n");
+	printf("error = %i\n", error);
+	printf("help_ptr = %p\n", help_ptr);
+	printf("\n");
+
+	// Use function getAdvancedHelpForKeywordW()
+	result_w = getAdvancedHelpForKeywordW(keyword_w, help_ptr);
+	printf("Obtained result (len=%d):\n", (NULL == result_w) ? -1 : (int)wcslen(result_w));
+	printf("--------------------------------------------------\n");
+	printf("%ws", (NULL == result_w) ? L"NULL" : result_w);
+	printf("--------------------------------------------------\n");
+	error = getTextFromFileW(text_result_filename_w, &text_result_ptr_w);
+	printf("After getTextFromFile:\n");
+	printf("error = %i\n", error);
+	printf("text_result_ptr = %p\n", help_ptr);
+	printf("Expected result (len=%d):\n", (NULL == text_result_ptr_w) ? -1 : (int)wcslen(text_result_ptr_w));
+	printf("--------------------------------------------------\n");
+	printf("%ws", (NULL == text_result_ptr_w) ? L"NULL" : text_result_ptr_w);
+	printf("--------------------------------------------------\n");
+
+
+	// Check against expected result
+	//printf("NULL == result --> %s\n", (NULL == result) ? "true" : "false");
+	//printf("NULL == text_result_ptr --> %s\n", (NULL == text_result_ptr) ? "true" : "false");
+	//printf("strlen(result)=%llu == strlen(text_result_ptr)=%llu --> %s\n", strlen(result), strlen(text_result_ptr), (strlen(result) == strlen(text_result_ptr)) ? "true" : "false");
+	//printf("0 == strcmp(result, text_result_ptr) --> %s\n", (0 == strcmp(result, text_result_ptr)) ? "true" : "false");
+	if (NULL != result_w && NULL != text_result_ptr_w && wcslen(result_w) == wcslen(text_result_ptr_w) && 0 == wcscmp(result_w, text_result_ptr_w)) {
+		printOK();
+	} else {
+		printf("Difference: %d", wcscmp(result_w, text_result_ptr_w));
+		printNOK();
+	}
+
+	// Clear variables
+	if (NULL != result_w) {
+		free(result_w);
+		result_w = NULL;
+	}
+	if (NULL != text_result_ptr_w) {
+		free(text_result_ptr_w);
+		text_result_ptr_w = NULL;
+	}
+	printf("\n");
+
+
+	// Free
+	printf("Before free:\n");
+	printf("help_ptr = %p\n", help_ptr);
+	freeAdvancedHelp(&help_ptr);
+	printf("After free:\n");
+	printf("help_ptr = %p\n", help_ptr);
+	printf("\n");
+
+
 }
